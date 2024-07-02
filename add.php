941d13e06349f970af00ca247088cd5043dcc87e
Rechnoe <temirlantashen4@gmail.com>
2024-07-03 00:29:41 +0500

    one-to-many app

--- add.php
+++ add.php
@@ -5,6 +5,6 @@
     echo '<a href="index.php">Click me to back</a>';
     $dbconn = pg_connect("host=localhost dbname=todo user=postgres password=")
         or die('Не удалось соединиться: ' . pg_last_error());
-    $query = "INSERT INTO todos (title, details) VALUES ('".$_POST['title']."', '".$_POST['details']."');";
+    $query = "INSERT INTO todos (title, details, user_id) VALUES ('".$_POST['title']."', '".$_POST['details']."', ".$_SESSION['user'].");";
     $result = pg_query($dbconn, $query) or die('Ошибка запроса: ' . pg_last_error());
 ?>
--- database.inc.php
+++ database.inc.php
@@ -1,4 +1,8 @@
 <?php
+    if (!session_id()) {
+        session_start();
+        session_regenerate_id();
+    }
     $fake_db = [];
     //Models
     include 'models.inc.php';
@@ -6,7 +10,7 @@
     // Соединение, выбор базы данных
     $dbconn = pg_connect("host=localhost dbname=todo user=postgres password=")
         or die('Не удалось соединиться: ' . pg_last_error());
-    $query = 'SELECT * FROM todos';
+    $query = 'SELECT * FROM todos WHERE user_id='.$_SESSION['user'].';';
     $result = pg_query($dbconn, $query) or die('Ошибка запроса: ' . pg_last_error());
     while( $row = pg_fetch_assoc($result) ) {
         $fake_db[] = new TodoTask($row['id'], $row['title'], $row['details']);
